75537812abb9d13d51bb80a00a0b467e894c27ca
Maksym Pavliv <pavliv.maksym@gmail.com>
2021-03-12 16:24:36 +0200

    some minor fixes

--- validator/user/index.js
+++ validator/user/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-    userAuthDataValidator: require('./user-auth-data.validator'),
     createUserValidator: require('./create-user.validator'),
     updateUserValidator: require('./update-user.validator'),
+    userAuthDataValidator: require('./user-auth-data.validator'),
     userSearchQueryValidator: require('./user-search-query.validator')
 };
